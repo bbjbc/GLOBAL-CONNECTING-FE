c56995b253463d050328de828fce135cecdbef80
bbjbc <jobc4550@gmail.com>
2024-07-13 21:59:52 +0900

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="m-5 flex items-center justify-center text-3xl font-extrabold text-yellow-400">
+        We are GLOBAL CONNECTING!
+      </h1>
+    </>
+  );
+};
+
+export default App;
