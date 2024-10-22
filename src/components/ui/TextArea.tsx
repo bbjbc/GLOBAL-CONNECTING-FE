9a4bd0c52636bb85b0ed638d7349e6e97c6ef5c3
bbjbc <jobc4550@gmail.com>
2024-10-23 00:20:51 +0900

    style: modify ui component

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,14 +1,19 @@
-import { TextareaHTMLAttributes } from 'react';
+import { forwardRef, TextareaHTMLAttributes } from 'react';
 
 type TextAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-const TextArea = (props: TextAreaProps) => {
-  return (
-    <textarea
-      className="w-full rounded-lg border border-gray-300 p-4 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1"
-      {...props}
-    />
-  );
-};
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className = '', ...props }, ref) => {
+    return (
+      <textarea
+        ref={ref}
+        className="w-full rounded-lg border border-gray-300 p-4 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1"
+        {...props}
+      />
+    );
+  },
+);
+
+TextArea.displayName = 'TextArea';
 
 export { TextArea };
