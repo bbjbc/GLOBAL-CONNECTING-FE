4d0202f3dcea2cb2702c000ce4e8037731cfe281
bbjbc <jobc4550@gmail.com>
2024-11-03 13:26:27 +0000

    chore: install types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/chat': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/chat': {
+          target: env.VITE_REVIEW_API,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
