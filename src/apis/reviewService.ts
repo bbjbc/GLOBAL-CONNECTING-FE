6d3f14c6f82da3a9eaff9cf4a6929ee90126c424
bbjbc <jobc4550@gmail.com>
2024-11-03 13:39:10 +0000

    refactor: 본 파일에 url 임포트

--- src/apis/reviewService.ts
+++ src/apis/reviewService.ts
@@ -3,11 +3,18 @@ import axios from 'axios';
 import { IReviewPayload, IReviewResponse } from '../types/review';
 import { generateReviewText } from '../mocks/mockReview';
 
-const apiUrl = '/chat/ask/1';
+const baseURL = import.meta.env.VITE_REVIEW_API;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 export const reviewService = {
   review: async (data: IReviewPayload): Promise<IReviewResponse> => {
-    const response = await axios.post<IReviewResponse>(apiUrl, data);
+    const response = await api.post<IReviewResponse>('/chat/ask/1', data);
     return response.data;
   },
 
